175a6a16dbf019e3ca24402066b645f43bdb33d9
RyutaroArita <7314603@ed.tus.ac.jp>
2015-02-23 17:46:46 +0900

    first commit

---
+++ src/com/example/map/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.map;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
